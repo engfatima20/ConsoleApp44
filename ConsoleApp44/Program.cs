c7b534232fd3252f0b2ecccf24391ec294357188
NV <NV@DESKTOP-832LD6A>
2025-05-24 16:45:12 +0300

    Add project files.

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name Please : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello!"+ name );
+
+        }
+    }
+}
